6f1a474d7a67040c7490528cc1fa80d65a06118c
Nataliia Tsirul <tcirulnatalya@gmail.com>
2025-02-19 10:08:41 +0000

    created function compare passwords with bcrypt

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
 
 const userSchema = new mongoose.Schema(
   {
@@ -42,5 +43,22 @@ const userSchema = new mongoose.Schema(
 );
 
 const User = mongoose.model('User', userSchema);
+
+// pre-save hook to hash  password before saving to database
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt); //from 123456 = adbgf;8f/t#
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+// 1234567 => invalid credentials
+userSchema.methods.comoarePassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
 
 export default User;
