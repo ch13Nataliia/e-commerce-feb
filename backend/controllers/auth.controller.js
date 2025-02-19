afa4628e9f78c2968194798c6b952d0da588d572
Nataliia Tsirul <tcirulnatalya@gmail.com>
2025-02-19 09:04:42 +0000

    first commit

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,12 @@
+export const signup = async (req, res) => {
+  res.send('Sign up ');
+};
+
+
+export const login = async (req, res) => {
+  res.send('Login up code');
+}
+
+export const logout = async (req, res) => {
+  res.send('Logout up code');
+}
---
+++ backend/server.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import authRoutes from "./routes/auth.route.js"
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use("/api/auth", authRoutes)
+
+app.listen(PORT, () => {
+  console.log(`Server listen on http://localhost: ${PORT} `);
+});
